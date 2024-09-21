f3ce9bae697af197a90e48467f5b07397c42b56b
sahiltambe18 <sahiltambe81922@gmail.com>
2024-09-21 16:22:53 +0530

    day 13

---
+++ palindromicSubstring.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        string ans = "";
+        int n = s.length();
+        if(n==1) return s;
+
+        for(int i = 0 ; i < n-1 ; i++){
+            // odd
+            int l = i , r = i;
+            while(l>=0 && r<n && s[l]==s[r]) l--,r++;
+            if(r-l-1>ans.length()){
+                ans = s.substr(l+1,r-l-1);
+            }
+            if(s[i]==s[i+1]){
+                l=i,r=i+1;
+                while(l>=0 && r<n && s[l]==s[r]) l--,r++;
+                if(r-l-1>ans.length()){
+                    ans = s.substr(l+1,r-l-1);
+                }
+                
+            }
+        }
+        return ans;
+    }
+};
+
+int main() {
+    Solution s;
+    string str = "babad";
+    cout<<s.longestPalindrome(str);
+    return 0;
+}
